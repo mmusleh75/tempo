8715da7d9edd55ac07035d232b934ef2fadb1f05
mmusleh <mmusleh75@gmail.com>
2017-06-19 12:00:21 -0500

    more and more

--- sp_refresh_time_tempo_data.sql
+++ sp_refresh_time_tempo_data.sql
@@ -135,6 +135,11 @@ BEGIN
 	WHERE Username IN ('sfarha','kbartholomae')
 	AND `Work Date` >= '2017-01-01' -- DO NOT CHANGE THE YEAR
 	AND `Project Key` != 'TM';
+	
+	DELETE FROM jiraanalysis.tmp_tempo_data
+	WHERE Username IN ('tmcdermott')
+	AND `Work Date` >= '2017-02-20' -- DO NOT CHANGE THE YEAR
+	;	
 /*
 	UPDATE jiraanalysis.tmp_tempo_data SET sprint = '16-01' WHERE `Work Date` BETWEEN '2015-12-31 00:00:00' AND '2016-01-13 23:59:59';
 	UPDATE jiraanalysis.tmp_tempo_data SET sprint = '16-02' WHERE `Work Date` BETWEEN '2016-01-14 00:00:00' AND '2016-01-27 23:59:59';
